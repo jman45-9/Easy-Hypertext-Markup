e2eb75c0d6b263b4a5f62a1646987dd5207f820a
Jeremy Cope <jcope0787@protonmail.com>
2024-08-29 12:06:37 -0700

    added paragraph content read

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,13 @@
 module Main where
 
+newParagraph :: [String] -> String
+newParagraph [] = "ERROR, MISSING SEMICOLON"
+newParagraph (x:ls) = case x of 
+        "paragraph" -> "<p>" ++ newParagraph ls 
+        "content" -> head ls ++ newParagraph (tail ls)
+        ";" -> "</p>"
+        _ -> "error, unknown keyword: " ++ x
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+        let test = newParagraph ["paragraph", "content", "lorem ipsum",";"]
+        print test
